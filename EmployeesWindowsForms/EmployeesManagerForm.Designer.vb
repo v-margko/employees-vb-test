fa6693824c5c2279ab36acb5c0d2a64b7e2b1de9
v-margko <v-margko@microsoft.com>
2018-04-10 20:10:56 +0300

    Added VB Windows Forms App (.Net Framework)

---
+++ EmployeesWindowsForms/EmployeesManagerForm.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class EmployeesManagerForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnGetAllEmployees = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.lblId = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.EmployeesListBox = New System.Windows.Forms.ListBox()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGetAllEmployees
+        '
+        Me.btnGetAllEmployees.Location = New System.Drawing.Point(12, 7)
+        Me.btnGetAllEmployees.Name = "btnGetAllEmployees"
+        Me.btnGetAllEmployees.Size = New System.Drawing.Size(112, 20)
+        Me.btnGetAllEmployees.TabIndex = 1
+        Me.btnGetAllEmployees.Text = "Get all employees"
+        Me.btnGetAllEmployees.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(249, 8)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(100, 20)
+        Me.txtSearch.TabIndex = 2
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(355, 7)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 3
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'lblId
+        '
+        Me.lblId.AutoSize = True
+        Me.lblId.Location = New System.Drawing.Point(442, 20)
+        Me.lblId.Name = "lblId"
+        Me.lblId.Size = New System.Drawing.Size(16, 13)
+        Me.lblId.TabIndex = 4
+        Me.lblId.Text = "Id"
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(445, 36)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(100, 20)
+        Me.txtId.TabIndex = 2
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(442, 59)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(57, 13)
+        Me.lblFirstName.TabIndex = 4
+        Me.lblFirstName.Text = "First Name"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(445, 75)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(100, 20)
+        Me.txtFirstName.TabIndex = 2
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(445, 114)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(100, 20)
+        Me.txtLastName.TabIndex = 2
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(445, 153)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(100, 20)
+        Me.txtEmail.TabIndex = 2
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(442, 98)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(58, 13)
+        Me.lblLastName.TabIndex = 4
+        Me.lblLastName.Text = "Last Name"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(442, 137)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(32, 13)
+        Me.lblEmail.TabIndex = 4
+        Me.lblEmail.Text = "Email"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(470, 196)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'EmployeesListBox
+        '
+        Me.EmployeesListBox.FormattingEnabled = True
+        Me.EmployeesListBox.Location = New System.Drawing.Point(12, 36)
+        Me.EmployeesListBox.Name = "EmployeesListBox"
+        Me.EmployeesListBox.Size = New System.Drawing.Size(418, 212)
+        Me.EmployeesListBox.TabIndex = 0
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(470, 225)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.btnRemove.TabIndex = 3
+        Me.btnRemove.Text = "Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(557, 261)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblId)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.btnGetAllEmployees)
+        Me.Controls.Add(Me.EmployeesListBox)
+        Me.Name = "Form1"
+        Me.Text = "Employees Manager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGetAllEmployees As Button
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents lblId As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents EmployeesListBox As ListBox
+    Friend WithEvents btnRemove As Button
+End Class
---
+++ EmployeesWindowsForms/EmployeesManagerForm.vb
@@ -0,0 +1,62 @@
+﻿Imports EmployeesLibrary
+Imports System.Configuration
+
+Public Class EmployeesManagerForm
+
+    Private Sub GetAllEmployees(sender As Object, e As EventArgs) Handles btnGetAllEmployees.Click
+        ClearList()
+        Dim emp As New EmployeesHelper(ConfigurationSettings.AppSettings.Get("EmployeesFilePath"))
+        Dim employees = emp.GetEmployees
+        For Each item As EmployeeModel In employees
+            EmployeesListBox.Items.Add(item)
+        Next
+    End Sub
+
+    Private Sub SearchEmployee(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim emp As New EmployeesHelper(ConfigurationSettings.AppSettings.Get("EmployeesFilePath"))
+        ClearList()
+        Dim search As String = txtSearch.Text
+        Dim searcId As Integer
+        Int32.TryParse(search, searcId)
+        Dim result As EmployeeModel = emp.GetEmployeeById(searcId)
+        If result Is Nothing Then
+            result = emp.GetEmployeeByName(search)
+            If result Is Nothing Then
+                result = emp.GetEmployeeByMail(search)
+                If result Is Nothing Then
+                    EmployeesListBox.Items.Add("Nothing was found")
+                Else EmployeesListBox.Items.Add(result)
+                End If
+            Else EmployeesListBox.Items.Add(result)
+            End If
+        Else EmployeesListBox.Items.Add(result)
+        End If
+    End Sub
+
+    Private Sub ClearList()
+        Dim count As Integer = EmployeesListBox.Items.Count
+        If count <> 0 Then
+            For index = count - 1 To 0 Step -1
+                EmployeesListBox.Items.RemoveAt(index)
+            Next
+        End If
+    End Sub
+
+    Private Sub AddEmployee(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim newEmp As EmployeeModel = New EmployeeModel
+        newEmp.Id = Convert.ToInt32(txtId.Text)
+        newEmp.FirstName = txtFirstName.Text
+        newEmp.LastName = txtLastName.Text
+        newEmp.Mail = txtEmail.Text
+        Dim emp As New EmployeesHelper(ConfigurationSettings.AppSettings.Get("EmployeesFilePath"))
+        emp.AddEmployee(newEmp)
+        GetAllEmployees(sender, e)
+    End Sub
+
+    Private Sub RemoveEmployee(sender As Object, e As EventArgs) Handles btnRemove.Click
+        Dim emp As New EmployeesHelper(ConfigurationSettings.AppSettings.Get("EmployeesFilePath"))
+        Dim empToRemove As EmployeeModel = EmployeesListBox.SelectedItem
+        emp.DeleteEmployee(empToRemove.Id)
+        GetAllEmployees(sender, e)
+    End Sub
+End Class
