b346cb196e9cd8f3362c2f63614bc845ca02ee9c
v-rodudi <v-rodudi@microsoft.com>
2018-05-03 17:33:35 +0300

    Added Unit Tests project.

---
+++ EmployeesTests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("EmployeesTests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("EmployeesTests")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("6d47e9c6-6d9b-4bae-8eae-c26f11ddcc0b")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ EmployeesTests/ThirdUnitTest.vb
@@ -0,0 +1,63 @@
+﻿Imports EmployeesLibrary
+
+<TestClass()>
+Public Class ThirdUnitTest
+    Private Shared Eh As EmployeesHelper
+    Private Shared Empl As EmployeeModel
+
+#Region "Additional test attributes"
+    '
+    ' You can use the following additional attributes as you write your tests:
+    '
+    ' Use ClassInitialize to run code before running the first test in the class
+    <ClassInitialize()> Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+        Eh = New EmployeesHelper("employees.json")
+        Empl = New EmployeeModel() With {
+            .Id = 999,
+            .FirstName = "TestFirstName999",
+            .LastName = "TestLastName999",
+            .Mail = "test999@mail.com"
+        }
+        Eh.AddEmployee(Empl)
+    End Sub
+    '
+    ' Use ClassCleanup to run code after all tests in a class have run
+    <ClassCleanup()> Public Shared Sub MyClassCleanup()
+        Eh.DeleteEmployee(999)
+    End Sub
+    '
+    ' Use TestInitialize to run code before running each test
+    ' <TestInitialize()> Public Sub MyTestInitialize()
+    ' End Sub
+    '
+    ' Use TestCleanup to run code after each test has run
+    ' <TestCleanup()> Public Sub MyTestCleanup()
+    ' End Sub
+    '
+#End Region
+
+    <TestMethod()>
+    Public Sub TestSearchById()
+        Dim Empl = Eh.GetEmployeeById(999)
+        Assert.AreEqual(ThirdUnitTest.Empl, Empl)
+    End Sub
+
+    <TestMethod()>
+    Public Sub TestSearchByFirstName()
+        Dim empl = Eh.GetEmployeeByName("TestFirstName999")
+        Assert.AreEqual(ThirdUnitTest.Empl, empl)
+    End Sub
+
+    <TestMethod()>
+    Public Sub TestSearchByLastName()
+        Dim empl = Eh.GetEmployeeByName("TestLastName999")
+        Assert.AreEqual(ThirdUnitTest.Empl, empl)
+    End Sub
+
+    <TestMethod()>
+    Public Sub TestSearchByMail()
+        Dim empl = Eh.GetEmployeeByMail("test999@mail.com")
+        Assert.AreEqual(ThirdUnitTest.Empl, empl)
+    End Sub
+
+End Class
